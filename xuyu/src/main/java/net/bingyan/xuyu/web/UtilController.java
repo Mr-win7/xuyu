9c450a9ac86184f164dfdf099fdfd5cec38647d3
Mr-win7 <wwq380820271@163.com>
2016-11-09 01:35:29 +0800

    add file support

--- xuyu/src/main/java/net/bingyan/xuyu/web/UtilController.java
+++ xuyu/src/main/java/net/bingyan/xuyu/web/UtilController.java
@@ -3,6 +3,7 @@ package net.bingyan.xuyu.web;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,20 +12,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
-@RequestMapping(value = "")
 public class UtilController extends BaseController {
 
 	@ResponseBody
 	@RequestMapping(value = "/image")
-	public String uploadImage(@RequestParam("photo") MultipartFile file) throws Exception {
+	public Map<String, Object> uploadImage(@RequestParam("photo") MultipartFile file) throws Exception {
 		if (!file.isEmpty()) {
 			String name = new SimpleDateFormat("yyyyMMddHHMMssSSS").format(new Date())
 					+ (int) (Math.random() * 10000000);
-			file.transferTo(new File("/home/wuwenqi/server/xuyu/image/" + name));
-			return name;
+			file.transferTo(new File(System.getProperty("user.home") + "/server/xuyu/image/" + name));
+			return pack(name);
 		} else {
-			// TODO
-			throw new Exception();
+			throw new Exception("empty file!");
 		}
 
 	}
