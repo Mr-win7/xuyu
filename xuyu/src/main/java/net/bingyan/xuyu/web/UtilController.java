5ea3125017dfaab03dbde7e9b8dc90edefda61f3
WuWenqi <WuWenqi@windows10.microdone.cn>
2016-11-08 17:47:40 +0800

    improve file support

--- xuyu/src/main/java/net/bingyan/xuyu/web/UtilController.java
+++ xuyu/src/main/java/net/bingyan/xuyu/web/UtilController.java
@@ -1,6 +1,8 @@
 package net.bingyan.xuyu.web;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
@@ -12,17 +14,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
-public class UtilController extends BaseController {
+public class UtilController extends BaseController
+{
 
 	@ResponseBody
 	@RequestMapping(value = "/image")
-	public Map<String, Object> uploadImage(@RequestParam("photo") MultipartFile file) throws Exception {
-		if (!file.isEmpty()) {
+	public Map<String, Object> uploadImage(@RequestParam("photo") MultipartFile file) throws Exception
+	{
+		if (!file.isEmpty())
+		{
 			String name = new SimpleDateFormat("yyyyMMddHHMMssSSS").format(new Date())
 					+ (int) (Math.random() * 10000000);
-			file.transferTo(new File(System.getProperty("user.home") + "/server/xuyu/image/" + name));
+			Path path = Paths.get(System.getProperty("user.home"), "server", "xuyu", "image", name);
+			file.transferTo(new File(path.toUri()));
 			return pack(name);
-		} else {
+		}
+		else
+		{
 			throw new Exception("empty file!");
 		}
 
